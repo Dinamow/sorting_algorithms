4ba7f5f2cb00c78814b88820731558fa45792d7d
DINAMOW <meemoo102039@gmail.com>
2023-07-25 21:40:12 +0000

    setting to zero

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -29,9 +29,8 @@ void selection_sort(int *array, size_t size)
 			array[i] = tmp;
 			array[index] = tmp1;
 			print_array(array, size);
-			tmp = 0;
-			tmp1 = 0;
 			index = 0;
+			tmp = 0;
 		}
 	}
 }
