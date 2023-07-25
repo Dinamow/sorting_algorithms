4cf1e9412896adb6a8bae42cc71ef4e28446f61a
DINAMOW <meemoo102039@gmail.com>
2023-07-25 21:53:39 +0000

    setting to zero

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,29 +9,26 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int tmp, tmp1;
+	int tmp1;
 	size_t i = 0, j = 0, index = 0;
 
 	if (size <= 0 || !size || !array)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		tmp = array[i];
 		tmp1 = array[i];
-		for (j = i; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
-			if (tmp > array[j])
+			if (tmp1 > array[j])
 			{
-				tmp = array[j];
 				index = j;
 			}
 		}
 		if (array[index] < tmp1)
 		{
-			array[i] = tmp;
 			array[index] = tmp1;
 			print_array(array, size);
-			tmp = 0;
+			tmp1 = 0;
 		}
 	}
 }
