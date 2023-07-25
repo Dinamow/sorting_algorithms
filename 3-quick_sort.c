140ca4f1509a81684fe57526fafea6d0223eb559
DINAMOW <meemoo102039@gmail.com>
2023-07-25 23:15:05 +0000

    test

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+ * quick_sort - sort func
+ * @array: array to sort
+ * @size: size of array
+ *
+ * Return: nothing
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	
+}
